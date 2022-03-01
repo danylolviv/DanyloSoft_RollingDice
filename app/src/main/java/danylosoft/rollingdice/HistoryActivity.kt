d6be5a3aa9ef5e673b7128bd707b8ff24d4fea29
tomaspeniak <71640446+pennyb0i@users.noreply.github.com>
2022-03-01 13:51:16 +0100

    history works lol

--- app/src/main/java/danylosoft/rollingdice/HistoryActivity.kt
+++ app/src/main/java/danylosoft/rollingdice/HistoryActivity.kt
@@ -1,8 +1,13 @@
 package danylosoft.rollingdice
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.appcompat.app.AppCompatActivity
@@ -15,12 +20,40 @@ class HistoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
 
-       val drawHistory = intent.getSerializableExtra("history")
+        returnBtn.setOnClickListener{ onBackPressed()}
 
-        /*val adapter : ListAdapter  = ArrayAdapter(MutableList<String>)(this,
-             android.R.layout.simple_list_item_1,
-             drawHistory) */
+       val history = intent.getSerializableExtra("history") as? HistoryManager
 
-        //   lvHistory.adapter = adapter
+
+        val adapter = HistoryAdapter(this, history!!.getList())
+
+           lvHistory.adapter = adapter
+    }
+
+    internal class HistoryAdapter(context: Context,
+                                 private val rolls: MutableList<String>
+    ) : ArrayAdapter<String>(context, 0, rolls)
+    {
+        // these colors are used to toogle the background of the list items.
+        private val colours = intArrayOf(
+            Color.parseColor("#AAAAAA"),
+            Color.parseColor("#CCCCCC")
+        )
+
+        override fun getView(position: Int, v: View?, parent: ViewGroup): View {
+            var v1: View? = v
+            if (v1 == null) {
+                val mInflater = LayoutInflater.from(context)
+                v1 = mInflater.inflate(R.layout.sample_cell_history, null)
+
+            }
+            val resView: View = v1!!
+            resView.setBackgroundColor(colours[position % colours.size])
+            val f = rolls[position]
+            val textView = resView.findViewById<TextView>(R.id.tvHistory)
+            textView.text = f
+
+            return resView
+        }
     }
 }
